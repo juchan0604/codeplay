54f9c869eaeb1489a981197d7c4984eb82ee8b96
juchan0604 <codepiay.kjc@gmail.com>
2025-10-10 18:52:37 +0900

    10/10 파이썬겜 1일차

---
+++ pygame_original/01_gameFrame.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import pygame
+pygame.init() # 초기화 (반드시 필요)
+
+#화면크기 설정
+screen_width = 500
+screen_height = 500
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+#화면 타이틀 (제목창)
+pygame.display.set_caption('코드플레이 만세')
+
+#이벤트 루프 - 종료까지 대기
+running = True #실행중인지 확인
+
+while running: # 반복
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+#종료처리
+pygame.quit()
---
+++ pygame_original/02_bg.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import pygame
+import random
+
+pygame.init() # 초기화 (반드시 필요)
+
+#화면크기 설정
+screen_width = 500 # 가로크기 
+screen_height = 500 # 세로크기
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+#화면 타이틀 (제목창)
+pygame.display.set_caption('피하기게임')
+
+#이미지 불러오기 (배경)
+bg = pygame.image.load('pygame_original/source/Iseeyou.png') #  Relative Path
+
+#이벤트 루프 - 종료까지 대기 
+running = True #실행중인지 확인
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        #                       Red                    Green                   Blue
+    screen.fill((random.randint(0,254), random.randint(0, 254), random.randint(0, 254))) # 배경을 이미지가 아닌 색으로 지정하는 방법 RGB
+    screen.blit(bg, (0, 0)) # blit = 배경 그리기
+    pygame.display.update() # 게임화면을 새로고침해줌
+
+#종료처리
+pygame.quit()
