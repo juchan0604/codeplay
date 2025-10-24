6d13a620387c63be9af5f88bfaf80e727a98b574
juchan0604 <codepiay.kjc@gmail.com>
2025-10-24 18:35:52 +0900

    10/24 프레임설정부터 충돌판정까지 - 주찬

---
+++ pygame_original/06_collision.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+import pygame
+import random
+
+pygame.init()#초기화 (반드시 필요)
+
+#화면크기 설정
+screen_width = 500 #가로크기
+screen_height = 500 #세로크기
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+#화면 타이틀 (제목창)
+pygame.display.set_caption("똥피하기-코드플레이")
+
+#FPS
+clock = pygame.time.Clock()
+
+#이동속도 고정해주기
+character_speed = 0.1
+enemy_speed = 1
+
+#이미지 불러오기
+bg = pygame.image.load("pygame_original/source/Iseeyou.png")#상대경로로 불러와야 다른 컴에서도 적용
+
+#스프라이트 불러오기
+character = pygame.image.load("pygame_original/source/gd.png")
+
+character_size = character.get_rect().size
+character_width = character_size[0]
+character_height = character_size[1]
+
+character_xPos = screen_width / 2 - character_width / 2
+character_yPos = screen_height  - character_height
+
+#두번쨰 스프라이트(적군) 생성
+enemy = pygame.image.load("pygame_original/source/enemy.png")
+#적군 스프라이트 크기 및 위치 지정
+enemy_size = enemy.get_rect().size #스프라이트를 사각형 형태로 가로세로 크기 구함
+enemy_width = enemy_size[0] #위에서 얻은 튜플의 1번째 값. 자동생성
+enemy_height = enemy_size[1] #위에서 얻은 튜플의 2번째 값. 자동생성
+enemy_xPos = random.randint(0, screen_width - enemy_width) #화면 가로 정중앙
+enemy_yPos = 0 #화면 세로 맨위
+
+#이동할 좌표 
+to_x = 0
+to_y = 0
+
+#이벤트 루프 - 종료까지 대기
+running = True #실행중인지 확인
+while running:
+    dt = clock.tick(30) #게임화면이 초당 리프레시되는 횟수
+
+    for event in pygame.event.get(): #키마 이벤트를 지속적으로 체크
+        if event.type == pygame.QUIT: #창닫는 이벤트
+            running = False
+        if event.type == pygame.KEYDOWN: #키보드 눌림 확인
+            if event.key == pygame.K_a: #왼쪽 화살표
+                to_x -= 1
+            elif event.key == pygame.K_d: #오른쪽 화살표
+                to_x += 1
+            elif event.key == pygame.K_w: #위쪽 화살표
+                to_y -= 1
+            elif event.key == pygame.K_s: #아래쪽 화살표 K_UP/K_DOWN?K_LEFT/K_RIGHT
+                to_y += 1
+        if event.type == pygame.KEYUP: # 키보드에서 손을 뗐을 떄 중지
+            if event.key == pygame.K_a or event.key == pygame.K_d: #가로움직임
+                to_x = 0
+            elif event.key == pygame.K_w or event.key == pygame.K_s: #세로움직임
+                to_y = 0
+
+    #추가한 이미지들을 화면에 띄우기
+    character_xPos += to_x * dt #스프라이트에 위치 반영
+    character_yPos += to_y * dt #스프라이트에 위치 반영
+    
+    # 가로 스크린내 안벗어나게
+    if character_xPos < 0:
+        character_xPos = 0
+    elif character_xPos > screen_width - character_width:
+        character_xPos = screen_width - character_width
+    
+    # 세로 스크린내 안벗어나게
+    if character_yPos < 0:
+        character_yPos = 0
+    elif character_yPos > screen_height - character_height:
+        character_yPos = screen_height - character_height
+
+    enemy_yPos += enemy_speed
+    if enemy_yPos > screen_height:
+        enemy_yPos = 0
+        enemy_speed = random.randint(10, 20)
+        enemy_xPos = random.randint(0, screen_width - enemy_width)
+
+    #충돌 처리하기
+    character_rect = character.get_rect() #get_rect는 가로세로 크기를 가져옴
+    character_rect.left = character_xPos #캐릭터의 실제좌표로 정보를 업데이트
+    character_rect.top = character_yPos #캐릭터의 실제좌표로 정보를 업데이트
+    
+    enemy_rect = enemy.get_rect()
+    enemy_rect.left = enemy_xPos
+    enemy_rect.top = enemy_yPos
+
+    # 충돌이벤트 체크
+    if character_rect.colliderect(enemy_rect): #collidefect는 두개의 사각형이 맞닿는지 체크하는 함수
+        print("CTASH!!!!!")
+        running = False
+
+
+    screen.blit(bg, (0, 0))#blit = 배경 그리기
+    screen.blit(character, (character_xPos, character_yPos)) #주인공 그리기
+    screen.blit(enemy, (enemy_xPos, enemy_yPos)) #적군 그리기 
+
+
+    pygame.display.update()# 게임화면을 새로고침해줌.
+
+#종료처리
+pygame.quit()
