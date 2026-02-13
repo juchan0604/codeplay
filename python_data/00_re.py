a53f031d8965ed6eb03e2a106737becc17e188bf
juchan0604 <codepiay.kjc@gmail.com>
2026-02-13 18:40:17 +0900

    2/13 데이터분석 첫걸음

---
+++ python_data/00_re.py
@@ -0,0 +1,9 @@
+import csv
+import matplotlib.pyplot as plt
+
+# plt.plot([0, 5, 10, 15, 20], [0, 100, 20, 30, 40])
+
+# plt.bar([0, 1, 2, 4, 6, 10], [-1, -2, -3, 5, 6, 7])
+plt.barh(range(6), [-1, -2, -3, 5, 6, 7])
+
+plt.show()
