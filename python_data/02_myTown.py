a768a126774a2d431023bbc1663569bb9a8aac57
juchan0604 <codepiay.kjc@gmail.com>
2026-03-06 19:57:07 +0900

    3/6 지역데이터 2개 비교 (while문 끝내기) 주찬

--- python_data/02_myTown.py
+++ python_data/02_myTown.py
@@ -7,3 +7,38 @@ yp = []
 
 for row in data:
     yp.append(row)
+    
+
+for i in yp:
+    for j in range(3, len(i)):
+        if ',' in i[j]:
+            i[j] = i[j].replace(",", "")
+        i[j] = int(i[j]) 
+running = True
+while running:
+    yp_select = []
+    yp_names = []
+    for bigyo in range(2):
+        area = input('지역명 입력 : ')
+        if area == '끝':
+            running = False
+            break
+        yp_names.append(area)
+        for local in yp:
+            if area in local[0]:
+                yp_select.append(local[3:])
+                break
+    if len(yp_names) == 2:
+        plt.rc('font', family='Malgun Gothic')
+        plt.title('양평군 인구분포도')
+        plt.style.use('ggplot')
+
+        area1 = f'{yp_names[0]} (총 인구수 : {sum(yp_select[0])})'
+        area2 = f'{yp_names[1]} (총 인구수 : {sum(yp_select[1])})'
+
+        plt.plot(yp_select[0], label = area1)
+        plt.plot(yp_select[1], label = area2)
+        plt.legend()
+        plt.show()
+
+print('goodbye')
